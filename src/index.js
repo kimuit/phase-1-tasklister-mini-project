8179d624ed706f7e808bf215beaf4c399645b08a
kimuit117 <kemoikiuita@gmail.com>
2022-09-06 00:50:13 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toMyList(e.target.newTask.value)
+    form.reset()
+  })
 });
+
+let toMyList = (todo) => {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteBtn)
+  btn.style.backgroundColor = 'red'
+  btn.textContent = 'x'
+  li.textContent = `${todo}  `
+  li.appendChild(btn)
+  console.log(li);
+  document.querySelector('#tasks').appendChild(li)
+}
+let deleteBtn = (e) => {
+  e.target.parentNode.remove()
+}
